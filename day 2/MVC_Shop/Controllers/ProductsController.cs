eeba78a477317a0957d8502a9dbadce5e9f583fa
Desai <2152757@cognizant.com>
2023-03-21 11:28:24 +0530

    commit

---
+++ day 2/MVC_Shop/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Shop.Models;
+namespace MVC_Shop.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        ProductsModel pObj = new ProductsModel();
+      public IActionResult ShowProducts()
+      {
+            return View(pObj.GetAllProducts());
+      }
+    }
+}
---
+++ day 2/MVC_Shop/Models/ProductsModel.cs
@@ -0,0 +1,26 @@
+﻿namespace MVC_Shop.Models
+{
+    public class ProductsModel
+    {
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+
+
+      private static List<ProductsModel> pList = new List<ProductsModel>()
+        {
+            new ProductsModel(){ pId=101, pName="Pepsi", pCategory="Cold-Drink", pIsInStock=true, pPrice=2},
+            new ProductsModel(){ pId=102, pName="Appy", pCategory="Cold-Drink", pIsInStock=true, pPrice=3},
+            new ProductsModel(){ pId=103, pName="IPhone", pCategory="Electronics", pIsInStock=false, pPrice=1900},
+            new ProductsModel(){ pId=104, pName="AirPods", pCategory="Electronics", pIsInStock=true, pPrice=750},
+            new ProductsModel(){ pId=105, pName="Nike", pCategory="Shoe", pIsInStock=false, pPrice=1250},
+        };
+
+        public List<ProductsModel> GetAllProducts()
+        {
+            return pList;
+        }
+    }
+}
