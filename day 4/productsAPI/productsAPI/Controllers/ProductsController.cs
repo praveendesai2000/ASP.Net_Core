79622d662d6978340f6a5b294c8d2a03ef573cb5
Desai <2152757@cognizant.com>
2023-03-21 11:33:46 +0530

    commit

---
+++ day 4/productsAPI/productsAPI/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using productsAPI.Models;
+using System.Security.Cryptography;
+
+namespace productsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        Products pObj = new Products();
+
+        [HttpGet]
+        [Route("plist")]
+        public IActionResult GetProducts()
+        {
+            return Ok(pObj.GetAllProducts());
+        }
+
+        [HttpGet]
+        [Route("plist/{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            try
+            {
+                return Ok(pObj.GetProductById(id));
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("plist/add")]
+        public IActionResult AddProduct(Products products)
+        {
+            return Created("", pObj.AddProduct(products));
+        }
+
+        [HttpDelete]
+        [Route("plist/delete/{pid}")]
+        public IActionResult DeleteProduct(int pid)
+        {
+            try
+            {
+                return Accepted(pObj.DeleteProduct(pid));
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("plist/update")]
+        public IActionResult UpdateProduct(Products changes)
+        {
+            try
+            {
+                return Accepted(pObj.updateProduct(changes));
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+    }
+
+}
---
+++ day 4/productsAPI/productsAPI/Models/Products.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Threading.Channels;
+
+namespace productsAPI.Models
+{
+    public class Products
+    {
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+
+        static List<Products> pList = new List<Products>()
+        {
+            new Products(){ pId=101, pName="Pepsi", pCategory="Cold-Drink" , pIsInStock=true, pPrice=50},
+            new Products(){ pId=102, pName="Iphone", pCategory="Electronic" , pIsInStock=true, pPrice=50},
+            new Products(){ pId=103, pName="Appy", pCategory="Cold-Drink" , pIsInStock=false, pPrice=50},
+            new Products(){ pId=104, pName="Green-Tea", pCategory="Health-Drink" , pIsInStock=true, pPrice=50},
+            new Products(){ pId=105, pName="Maggie", pCategory="Fast-Food" , pIsInStock=true, pPrice=50},
+            new Products(){ pId=106, pName="Pasta", pCategory="Fast-Food" , pIsInStock=false, pPrice=50},
+        };
+
+        public List<Products> GetAllProducts()
+        {
+            return pList;
+        }
+
+        public Products GetProductById(int id)
+        {
+            var p = (from pr in pList
+                     where pr.pId == id
+                     select pr).Single();
+
+            if (p != null)
+            {
+                return p;
+            }
+            throw new Exception("Product Not Found");
+        }
+
+        public string AddProduct(Products newProduct)
+        {
+            pList.Add(newProduct);
+            return "Product Added Successfully";
+        }
+
+        public string updateProduct(Products changes)
+        {
+            var p = (from pr in pList
+                     where pr.pId == changes.pId
+                     select pr).Single();
+            if (p != null)
+            {
+                p.pName = changes.pName;
+                p.pCategory = changes.pCategory;
+                p.pPrice = changes.pPrice;
+                p.pIsInStock = changes.pIsInStock;
+                return "Product Updated Successfully";
+            }
+            throw new Exception("Product Not Found");
+        }
+        public string DeleteProduct(int pid)
+        {
+            var p = (from pr in pList
+                     where pr.pId == pid
+                     select pr).Single();
+            if (p != null)
+            {
+                pList.Remove(p);
+                return "Product Not Found";
+            }
+            throw new Exception("Product Not Found");
+        }
+    }
+
+}
